ce133a62b28f530ada20a3041338da4f3950a184
toxilamer <toxilamer@mail.ru>
2022-10-27 11:49:27 +0700

    add 1000 rub bug

--- kurs_bot.py
+++ kurs_bot.py
@@ -3,6 +3,7 @@
 import telebot
 import requests
 import json
+from telebot import types  # для указание типов
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot('5666758445:AAFh8Rt879v1_w5kvokY5qoZ4RiFCE07JsM')
@@ -11,15 +12,31 @@ ALL_currencys = [
     'USD',
     'EUR',
     'TJS',
+    'RUB',
     'THB'
 ]
+
+my_currencys = [
+    'RUB',
+    'THB',
+    'TJS',
+    'USD',
+    'EUR'
+]
+
+
+def add_button(message_chat_id):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for my_currency in my_currencys:
+        button_currency = types.KeyboardButton(my_currency)
+        markup.add(button_currency)
+    bot.send_message(message_chat_id, 'Choise currency:', reply_markup=markup)
 
 
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    str_curs = '\n/'.join(ALL_currencys)
-    bot.send_message(message.chat.id, f'/Courses')
+    add_button(message.chat.id)
 
 
 # Проверка курса
@@ -28,22 +45,28 @@ def handle_text(message):
     result = requests.get('https://cdn.cur.su/api/latest.json')
     if result.status_code == 200:
         dict_result = json.loads(result.text)
-        currencyRUB = 'RUB'
-        kursRUB = dict_result['rates'][currencyRUB]
-        if message.text == '/Courses':
+        my_currency = message.text
+        rates = dict_result['rates']
+        if my_currency in rates:
+            my_kurs = rates[my_currency]
             str_message_1 = ''
             str_message_1000 = ''
             for cur in ALL_currencys:
-                if cur != 'USD':
+                if cur == my_currency:
+                    continue
+                elif cur != 'USD':
                     kurs2 = dict_result['rates'][cur]
-                    kurs = kursRUB / kurs2
+                    kurs = my_kurs / kurs2
                 else:
-                    kurs = kursRUB
-                str_message_1 += f'{cur} to {currencyRUB}: {kurs:0.2f}\n'
-                str_message_1000 += f'{cur} to {currencyRUB} (1000 руб.): {1000 / kurs:0.0f}\n'
+                    kurs = my_kurs
+                str_message_1 += f'{cur} to {my_currency}: {kurs:0.2f}\n'
+                str_message_1000 += f'{cur} to {my_currency} (1000 {my_currency}): {1000 / kurs:0.0f}\n'
             bot.send_message(message.chat.id, str_message_1 + str_message_1000)
+        else:
+            bot.send_message(message.chat.id, f'Currency <{my_currency}> not found')
     else:
         bot.send_message(message.chat.id, f'Ошибка {result.status_code}')
+    add_button(message.chat.id)
 
 
 # Запускаем бота
